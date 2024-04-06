879eb428952624a5b658221206057b2bbe8bc825
Zmaster <40079303+izyq@users.noreply.github.com>
2024-04-06 23:02:06 +0800

    Add files via upload

---
+++ twitter.js
@@ -0,0 +1,18 @@
+/**
+
+twitter
+
+[rewrite_local]
+^https:\/\/twitter\.com.* url script-response-header https://github.com/izyq/QuanX/raw/main/twitter-header.js
+
+[mitm]
+hostname = twitter.com
+
+
+ */
+
+var modifiedHeaders = $response.headers;
+
+delete modifiedHeaders['content-security-policy']
+
+$done({ headers : modifiedHeaders});
